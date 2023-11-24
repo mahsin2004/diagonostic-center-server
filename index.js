b7c96d14fb55d56790e381397709afdb908450f0
mahsin <mahsin1912@gmail.com>
2023-11-25 05:08:55 +0600

    Jwt token create route and when user logout token clear

--- index.js
+++ index.js
@@ -7,39 +7,37 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleWare
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5000"
+  ],
+  credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
 
-// {
-//     origin: [
-//       // "https://bistro-boss-client-1.web.app",
-//       // "https://bistro-boss-restaurant-server-ashy.vercel.app",
-//       "http://localhost:5173",
-//       "http://localhost:5000"
-//     ],
-//     credentials: true,
-//   }
 
-//Custom middleware
-// const verifyToken = (req, res, next) => {
-//   const token = req?.cookies?.token;
-//   if (!token) {
-//     return res.status(401).send({ message: "unauthorized access" });
-//   }
-//   jwt.verify(token, process.env.USER_ACCESS_TOKEN, function (err, decoded) {
-//     if (err) {
-//       return res.status(401).send({ message: "unauthorized access" });
-//     }
-//     req.user = decoded;
-//     console.log(decoded)
-//     next();
-//   });
-// };
+// Custom middleware
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies?.token;
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  jwt.verify(token, process.env.USER_ACCESS_TOKEN, function (err, decoded) {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.user = decoded;
+    console.log(decoded)
+    next();
+  });
+};
+
+ // Verify Admin Custom Middleware
 
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.mowydsq.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -48,7 +46,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -56,22 +54,80 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const userCollection = client.db("medicalDB").collection("users");
+
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.user.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === "admin";
+      if (!isAdmin) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      next();
+    };
+   
+    //Routes
+    app.get("/users/admin/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+      console.log(req.user.email)
+      if (email !== req.user.email) {
+        return res.status(403).send({ message: "Forbidden Access" });
+      }
+
+      const query = { email: email };
+
+        const user = await userCollection.findOne(query);
+        let admin = false;
+
+        if (user) {
+          admin = user.role === "Admin";
+        }
+
+        res.send({ admin });
+    });
+
+    app.post("/users", async(req, res) => {
+      const user = req.body;
+      console.log(user);
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
 
 
+    // Create Json Web Token
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.USER_ACCESS_TOKEN, {
+        expiresIn: "365d",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "none",
+        })
+        .send({ success: true });
+    });
 
+    // Clear Json Web Token
+    app.post("/logOut", async (req, res) => {
+      const user = req.body;
+      console.log('log out user', user)
+      res.clearCookie("token", {maxAge: 0, sameSite: 'none', secure: true}).send({ success: true });
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
-
-
-
 
 app.get("/", (req, res) => {
   res.send("Server is Running");
